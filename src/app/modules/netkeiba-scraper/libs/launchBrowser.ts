e64238bcf6e6402cd3472be52b366920bca1d25a
tocomi <tocomi0112@gmail.com>
2023-06-18 22:28:48 +0900

    feat: update puppeteer option

--- src/app/modules/netkeiba-scraper/libs/launchBrowser.ts
+++ src/app/modules/netkeiba-scraper/libs/launchBrowser.ts
@@ -2,7 +2,7 @@ import puppeteer from 'puppeteer';
 
 export const launchBrowser = async () => {
   console.log('🏇 Launching browser...');
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: 'new' });
   const page = await browser.newPage();
   return { browser, page };
 };
