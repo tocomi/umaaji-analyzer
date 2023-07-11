56a3a9add3756d2329c705bd5c5f3eb292bab232
tocomi <tocomi0112@gmail.com>
2023-07-12 01:55:26 +0900

    feat: add revalidation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { RaceSummary } from './types';
 import { Races } from '@/races/Races';
 
+export const revalidate = 6000;
+
 export const getRaceSummaries = async (): Promise<RaceSummary[]> => {
   const response = await fetch('http://localhost:3000/api/races');
   const raceSummaries = (await response.json()) as RaceSummary[];
