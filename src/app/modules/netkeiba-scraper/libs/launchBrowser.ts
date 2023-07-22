b275c8888ac47ec9f54dc0abfefe0ef44a4de3d3
tocomi <tocomi0112@gmail.com>
2023-07-23 00:48:23 +0900

    feat: launch browser on vercel

--- src/app/modules/netkeiba-scraper/libs/launchBrowser.ts
+++ src/app/modules/netkeiba-scraper/libs/launchBrowser.ts
@@ -1,8 +1,28 @@
+import chromium from 'chrome-aws-lambda';
 import puppeteer from 'puppeteer';
+
+const isVercel = process.env.ENV_TYPE === 'vercel';
+
+const launchBrowserOnLocal = async () => {
+  return puppeteer.launch({ headless: 'new' });
+};
+
+const launchBrowserOnVercel = async () => {
+  return chromium.puppeteer.launch({
+    args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: true,
+    ignoreHTTPSErrors: true,
+  });
+};
 
 export const launchBrowser = async () => {
   console.log('🏇 Launching browser...');
-  const browser = await puppeteer.launch({ headless: 'new' });
+  const _launchBrowser = isVercel
+    ? launchBrowserOnVercel
+    : launchBrowserOnLocal;
+  const browser = await _launchBrowser();
   const page = await browser.newPage();
   return { browser, page };
 };
